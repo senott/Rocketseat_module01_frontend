51760e7baa48b2fb428878ed31da39a7ce6fe086
Senott <senott@yahoo.com>
2020-08-19 11:46:47 -0300

    Return projects from backend API.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import Header from './components/Header';
 import './App.css'
-import backgroundImage from './assets/background.jpg';
 
 function App() { 
 
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data)
+    })
+  }, []);
 
   function handleAddProject() { 
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -17,10 +23,8 @@ function App() {
     <>
       < Header title="Projects" />
 
-      <img src={ backgroundImage } alt="Landscape" width={500}/>
-
       <ul>
-        { projects.map(project => <li key={ project }>{ project }</li>) }
+        { projects.map(project => <li key={ project.id }>{ project.title }</li>) }
       </ul>
 
       <button type="button" onClick={ handleAddProject }>Adicionar projeto</button>
